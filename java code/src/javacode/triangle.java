bbc7543594e17cc37798b066f6094c2bb032f27e
Hii <Hii@192.168.1.10>
2018-08-26 23:18:54 -0700

    commit2

---
+++ java code/src/javacode/triangle.java
@@ -0,0 +1,17 @@
+package javacode;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,j;
+for(i=0;i<5;i++)
+{
+for(j=0;j<=i;j++) {
+	System.out.print("*");
+}	
+System.out.println();
+}
+	}
+
+}
