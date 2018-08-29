ba62a5fc3a118651b44b4d40a6196b762601014c
Hii <Hii@192.168.1.5>
2018-08-29 03:34:38 -0700

    commit 5

---
+++ swap_proj/src/swap_pack/swap_class.java
@@ -0,0 +1,19 @@
+package swap_pack;
+
+public class swap_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10,y=20,z;
+System.out.println("values of x and y before swapping");
+System.out.println("x=");System.out.print(x);
+System.out.println("\ny=");System.out.print(y);
+z=x;
+x=y;
+y=z;
+System.out.println("\nvalues of x and y after swapping");
+System.out.println("x=");System.out.print(x);
+System.out.println("\ny=");System.out.print(y);
+	}
+
+}
